16a1f2a896a9f6b6cd8a3d1bc689f97a1424e3fb
Thirunavukkarasu T <97720002+thiru042004@users.noreply.github.com>
2024-07-22 22:30:03 +0530

    ADD. Connection Established to MongoDB Atlas.

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require('express') 
 const app = express()
 
-const PORT = 3500
+const { PORT } = require('./configuration/config')
+const connect = require('./database/connection')
 
 app.get('/', (request, response) => {
     response.status(200).send({ message: "API running successfully !"})
 })
 
-app.listen(PORT, console.log(`Server is running at http://localhost:${PORT}`))
+connect()
+    .then(() => {
+        try{
+            app.listen(PORT, console.log(`Server is running at http://localhost:${PORT}`))
+        }
+        catch(error) {
+            console.log(`Can't connect to DB : ${error}`)
+        }
+    })
+    .catch((error) => {
+        console.log(`Error while connecting to database : ${error}`)
+    })
---
+++ database/connection.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose') 
+
+const { DB_URI } = require('../configuration/config')
+
+const connectToDatabase = async () => {
+    const db = await mongoose.connect(DB_URI)
+    console.log(`Connected successfully to database : ${db.connection.name}`)
+}
+
+module.exports = connectToDatabase
